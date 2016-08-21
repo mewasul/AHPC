9d751cbeb8d10d2dfee144b2f19fcd9b8ffb49d8
Åshild <aashildte@gmail.com>
2016-08-21 13:47:30 +1000

    Using numpy

--- find_stencil.py
+++ find_stencil.py
@@ -4,27 +4,20 @@
 from sympy.matrices import *
 from sympy import factorial
 from scipy.linalg import solve
+import numpy as np
 
 M = 3;
 N = 2*M+1;
 
-A = zeros(N,N)
-d = zeros(N)
+A = np.zeros((N,N))
+d = np.zeros(N)
 
 d[1] = 1;           # diff order 1
 
 for i in range(N):
     for j in range(M+1):
-         A[i,j] = A[i,-(j+1)] = (M-j)**i/(factorial(i))   # values of matrix
+         A[i,j] = A[i,-(j+1)] = (M-j)**i/(float)(factorial(i))   # values of matrix
          A[i,j] *= (-1)**(i*(j+1))                                     # alternating sign
 
-
-print A
-"""
-print det(A[:1,:1])
-print det(A[:2,:2])
-print det(A[:3,:3])
-print det(A[:4,:4])
-print det(A[:5,:5])
-print det(A[:6,:6])
-"""
+a = np.linalg.solve(A, d)
+print a
